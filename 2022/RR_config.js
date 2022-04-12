f2c2254b1aca2fddc5103cc80641099ae864d398
River Maddox <drogmadd706@gmail.com>
2022-04-12 19:17:14 -0400

    Adding Files for Team and Match Info at new Event

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -18,7 +18,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022mabos",
+        "defaultValue":"2022necmp1",
         "required":"true",
         "disabled":"true"
       },
