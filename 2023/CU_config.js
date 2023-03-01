b48af61e0aee2a6afa5fd337c696d0d802511f3f
Legoman1342 <lex.w.johnson@gmail.com>
2023-03-01 18:42:52 -0500

    Changed the default value of the Event question

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -20,7 +20,7 @@ var config_data =
         "code":"e",
         "title":"Event",
         "type":"event",
-        "defaultValue":"2023week0",
+        "defaultValue":"2023nhgrs",
         "disabled":"true"
       },
       "Match Level":{
