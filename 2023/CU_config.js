c3145ca54e4ec114abfa27fcd9ce7e2a68def7f2
UnTaintedSnail <98992304+UnTaintedSnail@users.noreply.github.com>
2023-03-30 10:04:45 -0400

    Glowing buttons and removing extra config.js files

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -20,7 +20,7 @@ var config_data =
         "code":"e",
         "title":"Event",
         "type":"event",
-        "defaultValue":"2023nhgrs",
+        "defaultValue":"2023cthars",
         "disabled":"true"
       },
       "Match Level":{
