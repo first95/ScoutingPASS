1030d4eb92d8450d64dbe9c0eae5634950dcbdf2
Legoman1342 <lex.w.johnson@gmail.com>
2023-03-31 23:39:46 -0400

    Changed the event code in a few places

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -20,7 +20,7 @@ var config_data =
         "code":"e",
         "title":"Event",
         "type":"event",
-        "defaultValue":"2023cthars",
+        "defaultValue":"2023cthar",
         "disabled":"true"
       },
       "Match Level":{
