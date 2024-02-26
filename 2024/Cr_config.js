69831f2e0a789446d200d81d9a61a7cf2a610d85
UnTaintedSnail <98992304+UnTaintedSnail@users.noreply.github.com>
2024-02-26 18:57:00 -0500

    New 2024 Data Input Config

--- 2024/Cr_config.js
+++ 2024/Cr_config.js
@@ -20,7 +20,7 @@ var config_data =
         "code":"e",
         "title":"Event",
         "type":"event",
-        "defaultValue":"2023necmp2",
+        "defaultValue":"2024nhgrs",
         "disabled":"true"
       },
       "Match Level":{
@@ -37,7 +37,7 @@ var config_data =
         "title":"Match Number",
         "type":"match",
         "min":1,
-        "max":100
+        "max":78
       },
      "Robot": {
         "code":"r",
@@ -68,22 +68,206 @@ var config_data =
       },
       "Auton Exits Community?": {
         "code":"ae",
-        "title":"Exits Community?",
+        "title":"Leaves Startline?",
         "type":"bool"
       },
-      "Auton Speaker Scored": {
-        "code":"aa",
-        "title": "Amp Notes Scored",
-        "type":"counter",
-        "min":0,
-        "max":10
+      "FirstAutoNote": {
+        "code": "FiN",
+        "title": "Ring Num/Place",
+        "type": "dropdown",
+        "columns": [
+          {"title": "Amp",
+          "choices": {
+            "NA":"Amp",
+            "A0": "0",
+            "A1": "1",
+            "A2": "2",
+            "A3": "3",
+            "A4": "4",
+            "A5": "5",
+            "A6": "6",
+            "A7": "7",
+            "A8": "8"
+          }},
+          {"title": "Spkr",
+          "choices": {
+            "NA":"Spkr",
+            "S0": "0",
+            "S1": "1",
+            "S2": "2",
+            "S3": "3",
+            "S4": "4",
+            "S5": "5",
+            "S6": "6",
+            "S7": "7",
+            "S8": "8"
+          }}
+        ]
       },
-      "Auton Amp Scored": {
-        "code":"as",
-        "title": "Speaker Notes Scored",
-        "type":"counter",
-        "min":0,
-        "max":10
+      "SecondAutoNote": {
+        "code": "SeN",
+        "title": "Ring Num/Place",
+        "type": "dropdown",
+        "columns": [
+          {"title": "Amp",
+          "choices": {
+            "NA":"Amp",
+            "A0": "0",
+            "A1": "1",
+            "A2": "2",
+            "A3": "3",
+            "A4": "4",
+            "A5": "5",
+            "A6": "6",
+            "A7": "7",
+            "A8": "8"
+          }},
+          {"title": "Spkr",
+          "choices": {
+            "NA":"Spkr",
+            "S0": "0",
+            "S1": "1",
+            "S2": "2",
+            "S3": "3",
+            "S4": "4",
+            "S5": "5",
+            "S6": "6",
+            "S7": "7",
+            "S8": "8"
+          }}
+        ]
+      },
+      "ThirdAutoNote": {
+        "code": "ThN",
+        "title": "Ring Num/Place",
+        "type": "dropdown",
+        "columns": [
+          {"title": "Amp",
+          "choices": {
+            "NA":"Amp",
+            "A0": "0",
+            "A1": "1",
+            "A2": "2",
+            "A3": "3",
+            "A4": "4",
+            "A5": "5",
+            "A6": "6",
+            "A7": "7",
+            "A8": "8"
+          }},
+          {"title": "Spkr",
+          "choices": {
+            "NA":"Spkr",
+            "S0": "0",
+            "S1": "1",
+            "S2": "2",
+            "S3": "3",
+            "S4": "4",
+            "S5": "5",
+            "S6": "6",
+            "S7": "7",
+            "S8": "8"
+          }}
+        ]
+      },
+      "FourthAutoNote": {
+        "code": "FoN",
+        "title": "Ring Num/Place",
+        "type": "dropdown",
+        "columns": [
+          {"title": "Amp",
+          "choices": {
+            "NA":"Amp",
+            "A0": "0",
+            "A1": "1",
+            "A2": "2",
+            "A3": "3",
+            "A4": "4",
+            "A5": "5",
+            "A6": "6",
+            "A7": "7",
+            "A8": "8"
+          }},
+          {"title": "Spkr",
+          "choices": {
+            "NA":"Spkr",
+            "S0": "0",
+            "S1": "1",
+            "S2": "2",
+            "S3": "3",
+            "S4": "4",
+            "S5": "5",
+            "S6": "6",
+            "S7": "7",
+            "S8": "8"
+          }}
+        ]
+      },
+      "FifthAutoNote": {
+        "code": "FfN",
+        "title": "Ring Num/Place",
+        "type": "dropdown",
+        "columns": [
+          {"title": "Amp",
+          "choices": {
+            "NA":"Amp",
+            "A0": "0",
+            "A1": "1",
+            "A2": "2",
+            "A3": "3",
+            "A4": "4",
+            "A5": "5",
+            "A6": "6",
+            "A7": "7",
+            "A8": "8"
+          }},
+          {"title": "Spkr",
+          "choices": {
+            "NA":"Spkr",
+            "S0": "0",
+            "S1": "1",
+            "S2": "2",
+            "S3": "3",
+            "S4": "4",
+            "S5": "5",
+            "S6": "6",
+            "S7": "7",
+            "S8": "8"
+          }}
+        ]
+      },
+      "SixthAutoNote": {
+        "code": "SiN",
+        "title": "Ring Num/Place",
+        "type": "dropdown",
+        "columns": [
+          {"title": "Amp",
+          "choices": {
+            "NA":"Amp",
+            "A0": "0",
+            "A1": "1",
+            "A2": "2",
+            "A3": "3",
+            "A4": "4",
+            "A5": "5",
+            "A6": "6",
+            "A7": "7",
+            "A8": "8"
+          }},
+          {"title": "Spkr",
+          "choices": {
+            "NA":"Spkr",
+            "S0": "0",
+            "S1": "1",
+            "S2": "2",
+            "S3": "3",
+            "S4": "4",
+            "S5": "5",
+            "S6": "6",
+            "S7": "7",
+            "S8": "8"
+          }}
+        ]
       },
     },
     "teleop": {
@@ -92,16 +276,23 @@ var config_data =
         "title":"Teleop",
         "type":"divider"
       },
-      "Teleop Objects Scored": {
+      "Teleop Amp Notes Scored": {
         "code":"ta",
         "title": "Amp Notes Scored",
         "type":"counter",
         "min":0,
         "max":50
       },
-      "Teleop Amp Notes Scored": {
-        "code":"ts",
-        "title": "Speaker Notes Scored",
+      "Teleop Muffled Speaker Notes Scored": {
+        "code":"tms",
+        "title": "Muffled Speaker Notes",
+        "type":"counter",
+        "min":0,
+        "max":50
+      },
+      "Teleop Amped Speaker Notes Scored": {
+        "code":"tas",
+        "title": "Amped Speaker Notes",
         "type":"counter",
         "min":0,
         "max":50
@@ -182,6 +373,11 @@ var config_data =
           "Not Observed":"Not Observed"
         },
         "defaultValue":"Not Observed"
+      },
+      "Button Presser": {
+        "code":"th",
+        "title":"Button Presser?",
+        "type":"bool"
       },
       "Comments": {
         "code":"co",
