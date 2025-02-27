bf41a89eab668205e396fe3e3bcb61a576eeff54
zaaj <adam.a.johnson@gmail.com>
2025-02-27 07:58:02 -0500

    Removing old and unused files

--- 2025/Reefscape_config.js
+++ 2025/Reefscape_config.js
@@ -20,7 +20,7 @@ var config_data =
         "code":"e",
         "title":"Event",
         "type":"event",
-        "defaultValue":"2025necmp2",
+        "defaultValue":"2025mefal",
         "disabled":"true"
       },
       "Match Number":{
