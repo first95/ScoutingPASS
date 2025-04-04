4ce74906b58810ffd914978bab86702062fafd7c
zaaj <adam.a.johnson@gmail.com>
2025-04-04 17:32:34 -0400

    Pulled in Statbotics.io data too - intended for easy viewing in out-gridview

--- resources/ParseData.ps1
+++ resources/ParseData.ps1
@@ -1,4 +1,12 @@
  $matchdata = ipcsv .\2025necmp1.tsv -Delimiter "`t"
+
+ $insights = import-csv .\2025necmp1_team_insights.csv
+ $nameOf = @{}
+ $epaOf = @{}
+ $insights | foreach-object {
+	$nameOf[$_.num] = $_.team
+	$epaOf[$_.num] = "   Auto: $($_.auto_epa)`n   Tele: $($_.teleop_epa)`nEndgame: $($_.endgame_epa)`n  Total: $($_.total_epa)"
+ }
     
  $barge = @{N="Barge";e={(($_.group |? barge_status).barge_status | group | % {"$($_.count) $($_.name)"} ) -join "`n"}}
  $AutonStart = @{N="AutonStart";e={(($_.group |? auton_start_position).auton_start_position | group | % {"$($_.count) $($_.name)"} ) -join "`n"}}
@@ -16,4 +24,13 @@ $coralLevelsObserved = @{N="# Matches Coral Levels observed"; E={$_.group | % {
 	
 	}}
  
- $matchdata | group team_number | select name, $barge, $autonStart, $comments, $bargeAlgaeScored, $ProcessorAlgaeScored, $coralLevelsObserved | sort {[int]$_.name} | convertto-csv -notypeinformation
+ $matchdata | group team_number | select @{N="TeamNumber";E={$_.name}}	,
+										 @{N="TeamName";E={$nameOf[$_.name]}},
+										 @{N="EPAs";E={$epaOf[$_.name]}},
+ 										 $barge, 
+										 $autonStart, 
+										 $comments, 
+										 $bargeAlgaeScored, 
+										 $ProcessorAlgaeScored, 
+										 $coralLevelsObserved `
+| sort {[int]$_.TeamNumber} | convertto-csv -notypeinformation
