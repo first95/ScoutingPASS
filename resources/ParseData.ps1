0ed842dac8d97bc2f0eb8104836196e5d320cbe8
zaaj <adam.a.johnson@gmail.com>
2025-04-04 17:53:36 -0400

    Tweaked column views, updated statbotics data

--- resources/ParseData.ps1
+++ resources/ParseData.ps1
@@ -2,19 +2,21 @@
 
  $insights = import-csv .\2025necmp1_team_insights.csv
  $nameOf = @{}
- $epaOf = @{}
+ $TotalEpaOf = @{}
+ $epasOf = @{}
  $insights | foreach-object {
 	$nameOf[$_.num] = $_.team
-	$epaOf[$_.num] = "   Auto: $($_.auto_epa)`n   Tele: $($_.teleop_epa)`nEndgame: $($_.endgame_epa)`n  Total: $($_.total_epa)"
+	$TotalEpaOf[$_.num] = $_.total_epa
+	$epasOf[$_.num] = "   Auto: $($_.auto_epa)`n   Tele: $($_.teleop_epa)`nEndgame: $($_.endgame_epa)"
  }
     
  $barge = @{N="Barge";e={(($_.group |? barge_status).barge_status | group | % {"$($_.count) $($_.name)"} ) -join "`n"}}
  $AutonStart = @{N="AutonStart";e={(($_.group |? auton_start_position).auton_start_position | group | % {"$($_.count) $($_.name)"} ) -join "`n"}}
  $comments = @{N="comments";e={($_.group |? comments | % {"$($_.match_number): $($_.comments)"}) -join "`n"}}
- $bargeAlgaeScored = @{N="BargeAlgaeScored";E={$measure = $_.group | measure-object -property Barge_Algae_Scored -max
- 										  "Max: $($measure.maximum)"}}
- $ProcessorAlgaeScored = @{N="ProcessorAlgaeScored";E={$measure = $_.group | measure-object -property Processor_Algae_Scored -max 
- 										  "Max: $($measure.maximum)"}}
+ $bargeAlgaeScored = @{N="MaxBargeAlgae";E={$measure = $_.group | measure-object -property Barge_Algae_Scored -max
+ 										  $measure.maximum }}
+ $ProcessorAlgaeScored = @{N="MaxProcessorAlgae";E={$measure = $_.group | measure-object -property Processor_Algae_Scored -max 
+ 										  $measure.maximum }}
 $coralLevelsObserved = @{N="# Matches Coral Levels observed"; E={$_.group | % { 
 	if ($_.Coral_Scored_on_L1 -eq "Yes") {"L1"}
 	if ($_.Coral_Scored_on_L2 -eq "Yes") {"L2"}
@@ -26,11 +28,12 @@ $coralLevelsObserved = @{N="# Matches Coral Levels observed"; E={$_.group | % {
  
  $matchdata | group team_number | select @{N="TeamNumber";E={$_.name}}	,
 										 @{N="TeamName";E={$nameOf[$_.name]}},
-										 @{N="EPAs";E={$epaOf[$_.name]}},
+										 @{N="TotalEpa";E={$TotalEpaOf[$_.name]}},
+										 @{N="EPAs";E={$epasOf[$_.name]}},
  										 $barge, 
 										 $autonStart, 
-										 $comments, 
 										 $bargeAlgaeScored, 
 										 $ProcessorAlgaeScored, 
-										 $coralLevelsObserved `
+										 $coralLevelsObserved,
+										 $comments `
 | sort {[int]$_.TeamNumber} | convertto-csv -notypeinformation
