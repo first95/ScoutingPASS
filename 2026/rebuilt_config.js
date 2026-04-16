4f5e9a934162230774852df735d736dc1c70b4ad
zaaj <adam.a.johnson@gmail.com>
2026-04-16 12:54:18 -0400

    changed config to actually point to the current event code

--- 2026/rebuilt_config.js
+++ 2026/rebuilt_config.js
@@ -20,7 +20,7 @@ var config_data =
         "code": "e",
         "title": "Event",
         "type": "event",
-        "defaultValue": "2026vtbur",
+        "defaultValue": "2026necmp2",
         "disabled": "true"
       },
       "Match Number": {
